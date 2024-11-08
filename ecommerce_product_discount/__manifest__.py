1c0209f1322d01fa9ecb927ba0bccb245cccbc09
Vikash Tiwari <vitiodoo@gmail.com>
2024-11-08 22:37:52 +0530

    [ADD] added code of ecommerce product discount price

---
+++ ecommerce_product_discount/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Product Discount for eCommerce',
+    'version': '15.0.1',
+    'summary': 'Discount percentage on eCommerce product based on discount showing the price',
+    'category': 'Sales',
+    'author': 'Vikash Tiwari',
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_template.xml',
+        'views/website_sale_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
