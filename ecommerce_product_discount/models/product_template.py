7d20452ec478bf1b81cffa93efa336bfd8acfb81
Vikash Tiwari <vitiodoo@gmail.com>
2024-11-08 22:40:11 +0530

    [ADD] added code of ecommerce product discount price updated code

--- ecommerce_product_discount/models/product_template.py
+++ ecommerce_product_discount/models/product_template.py
@@ -1,4 +1,9 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import AccessError, ValidationError, UserError
+import requests
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -24,7 +29,6 @@ class SaleOrder(models.Model):
         product_context = dict(self.env.context)
         product_context.setdefault('lang', self.sudo().partner_id.lang)
         SaleOrderLineSudo = self.env['sale.order.line'].sudo().with_context(product_context)
-        # change lang to get correct name of attributes/values
         product_with_context = self.env['product.product'].with_context(product_context)
         product = product_with_context.browse(int(product_id)).exists()
 
@@ -44,87 +48,60 @@ class SaleOrder(models.Model):
         quantity = 0
         order_line = False
         if self.state != 'draft':
-            request.session['sale_order_id'] = None
+            requests.session['sale_order_id'] = None
             raise UserError(_('It is forbidden to modify a sales order which is not in draft status.'))
         if line_id is not False:
             order_line = self._cart_find_product_line(product_id, line_id, **kwargs)[:1]
-
-        # Create line if no line with product_id can be located
         if not order_line:
             no_variant_attribute_values = kwargs.get('no_variant_attribute_values') or []
             received_no_variant_values = product.env['product.template.attribute.value'].browse([int(ptav['value']) for ptav in no_variant_attribute_values])
             received_combination = product.product_template_attribute_value_ids | received_no_variant_values
             product_template = product.product_tmpl_id
-
-            # handle all cases where incorrect or incomplete data are received
             combination = product_template._get_closest_possible_combination(received_combination)
-
-            # get or create (if dynamic) the correct variant
             product = product_template._create_product_variant(combination)
-
             if not product:
                 raise UserError(_("The given combination does not exist therefore it cannot be added to cart."))
-
             product_id = product.id
-
             values = self._website_product_id_change(self.id, product_id, qty=1)
-
-            # add no_variant attributes that were not received
             for ptav in combination.filtered(lambda ptav: ptav.attribute_id.create_variant == 'no_variant' and ptav not in received_no_variant_values):
                 no_variant_attribute_values.append({
                     'value': ptav.id,
                 })
-
-            # save no_variant attributes values
             if no_variant_attribute_values:
                 values['product_no_variant_attribute_value_ids'] = [
                     (6, 0, [int(attribute['value']) for attribute in no_variant_attribute_values])
                 ]
-
-            # add is_custom attribute values that were not received
             custom_values = kwargs.get('product_custom_attribute_values') or []
             received_custom_values = product.env['product.template.attribute.value'].browse([int(ptav['custom_product_template_attribute_value_id']) for ptav in custom_values])
-
             for ptav in combination.filtered(lambda ptav: ptav.is_custom and ptav not in received_custom_values):
                 custom_values.append({
                     'custom_product_template_attribute_value_id': ptav.id,
                     'custom_value': '',
                 })
-
-            # save is_custom attributes values
             if custom_values:
                 values['product_custom_attribute_value_ids'] = [(0, 0, {
                     'custom_product_template_attribute_value_id': custom_value['custom_product_template_attribute_value_id'],
                     'custom_value': custom_value['custom_value']
                 }) for custom_value in custom_values]
-
-            # create the line
             order_line = SaleOrderLineSudo.create(values)
 
             try:
                 order_line._compute_tax_id()
             except ValidationError as e:
-                # The validation may occur in backend (eg: taxcloud) but should fail silently in frontend
                 _logger.debug("ValidationError occurs during tax compute. %s" % (e))
             if add_qty:
                 add_qty -= 1
-
-        # compute new quantity
         if set_qty:
             quantity = set_qty
         elif add_qty is not None:
             quantity = order_line.product_uom_qty + (add_qty or 0)
-
-        # Remove zero of negative lines
         if quantity <= 0:
             linked_line = order_line.linked_line_id
             order_line.unlink()
             if linked_line:
-                # update description of the parent
                 linked_product = product_with_context.browse(linked_line.product_id.id)
                 linked_line.name = linked_line.get_sale_order_line_multiline_description_sale(linked_product)
         else:
-            # update line
             no_variant_attributes_price_extra = [ptav.price_extra for ptav in order_line.product_no_variant_attribute_value_ids]
             values = self.with_context(no_variant_attributes_price_extra=tuple(no_variant_attributes_price_extra))._website_product_id_change(self.id, product_id, qty=quantity)
             order = self.sudo().browse(self.id)
@@ -141,8 +118,6 @@ class SaleOrder(models.Model):
                 'discount':product.discount_percentage
             })
             order_line.write(values)
-
-            # link a product to the sales order
             if kwargs.get('linked_line_id'):
                 linked_line = SaleOrderLineSudo.browse(kwargs['linked_line_id'])
                 order_line.write({
@@ -150,13 +125,6 @@ class SaleOrder(models.Model):
                 })
                 linked_product = product_with_context.browse(linked_line.product_id.id)
                 linked_line.name = linked_line.get_sale_order_line_multiline_description_sale(linked_product)
-            # Generate the description with everything. This is done after
-            # creating because the following related fields have to be set:
-            # - product_no_variant_attribute_value_ids
-            # - product_custom_attribute_value_ids
-            # - linked_line_id
             order_line.name = order_line.get_sale_order_line_multiline_description_sale(product)
-
         option_lines = self.order_line.filtered(lambda l: l.linked_line_id.id == order_line.id)
-
         return {'line_id': order_line.id, 'quantity': quantity, 'option_ids': list(set(option_lines.ids))}
